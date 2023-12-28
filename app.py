e4da0feeb99e4886aee386ba55df7970d1e56281
Surav Shrestha <surav.shrestha@outlook.com>
2023-12-28 19:03:09 +0545

    chore: remove query persistence

--- app.py
+++ app.py
@@ -1,43 +1,23 @@
-import os
-
 from flask import Flask, request, jsonify
 
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
-from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.vectorstores import Chroma
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 
-# Enable to save to disk & reuse the model (for repeated queries on the same data)
-PERSIST = False
+# Load data
+loader = TextLoader("data/data.md")
 
-if PERSIST and os.path.exists("persist"):
-    print("Reusing index...\n")
-    # Reuse existing vector store index
-    vectorstore = Chroma(persist_directory="persist",
-                         embedding_function=OpenAIEmbeddings())
-    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-    # Load data and create a new vector store index
-    loader = TextLoader("data/data.md")
+# Use this line if you need to load a directory of files as source
+# loader = DirectoryLoader("data/")
 
-    # Use this line if you need to load a directory of files as source
-    # loader = DirectoryLoader("data/")
-
-    if PERSIST:
-        # Create and persist vector store index
-        index = VectorstoreIndexCreator(
-            vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
-    else:
-        # Create vector store index without persistence
-        index = VectorstoreIndexCreator().from_loaders([loader])
+# Create vector store index without persistence
+index = VectorstoreIndexCreator().from_loaders([loader])
 
 # Set up Conversational Retrieval Chain with OpenAI GPT-3.5 Turbo
 chain = ConversationalRetrievalChain.from_llm(
